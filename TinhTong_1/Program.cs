1e3ca4df6fe65ed9166acb67c493ff7a4b0c1157
Nguyen Minh Duy <duynguyendev810@gmail.com>
2018-01-18 10:41:10 +0700

    Update project TinhTong_1

---
+++ TinhTong_1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TinhTong_1
+{
+    class Program
+    {
+        //Đề : Tính tổng S = 1 + 2 + ..... + n với n được nhập từ người dùng
+        public static int TinhTong()
+        {
+            Console.Write("Nhap 1 so nguyen n bat ky : ");
+            int n = int.Parse(Console.ReadLine());
+            if (n < 0) return 0;
+                
+            int tong = 0;
+            for (int i = 0; i <= n; i++)
+            {
+                tong += i;
+            }
+            return tong;
+
+        }
+
+        static void Main(string[] args)
+        {
+            int res = TinhTong();
+            Console.WriteLine("Tong = " + res);
+            Console.ReadKey();
+        }
+    }
+}
